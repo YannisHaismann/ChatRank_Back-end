ffa615c85b4dd604686670e19264260e7dbfe0d5
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2022-04-19 17:19:22 +0200

    JWTSubscriber v1.0

---
+++ src/EventSubscriber/JWTSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class JWTSubscriber implements EventSubscriberInterface
+{
+    public function onLexikJwtAuthenticationOnJwtCreated($event)
+    {
+       $data = $event->getData();
+       $data['username'] = $event->getUser()->getUsername();
+       $data['roles'] = $event->getUser()->getRoles();
+       $event->setData($data);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'lexik_jwt_authentication.on_jwt_created' => 'onLexikJwtAuthenticationOnJwtCreated',
+        ];
+    }
+}
